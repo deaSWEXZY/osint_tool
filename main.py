6de38031a2c0eedc64af201879bffafc7f17775a
deaSWEXZY <davidbrown1235710@gmail.com>
2026-06-18 12:31:54 +0400

    Functional Update.

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from modules import SiteSearch
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+print(Fore.CYAN + Style.BRIGHT + """
+  ___  ____ ___ _   _ _____ 
+ / _ \/ ___|_ _| \ | |_   _|
+| | | \___ \| ||  \| | | |  
+| |_| |___) | || |\  | | |  
+ \___/|____/___|_| \_| |_|  
+      Username Scanner
+""" + Style.RESET_ALL)
 
 # ----------- USER INPUT -----------
 USER_NAME = input("Please input, the exact username: ")
 SITE_TO_SEARCH = input("Please Input the site for searching information: ")
+SAVE_RESULTS = input("Save results in memory?\nY / N: ").lower()
 
-scanner = SiteSearch(USER_NAME, SITE_TO_SEARCH)
+save_or_not = False
+
+if SAVE_RESULTS == 'y':
+    save_or_not = True
+elif SAVE_RESULTS == 'n':
+    save_or_not = False
+else:
+    print("Please type 'Y' or 'N'...")
+
+scanner = SiteSearch(USER_NAME, SITE_TO_SEARCH, save_or_not)
 scanner.run_all()
--- modules.py
+++ modules.py
@@ -1,51 +1,76 @@
 import requests
 import json
-from colorama import Fore, init, Style
+from colorama import Fore, init
+import os
+import tempfile
 
+init(autoreset=True) #Colorama Color Reset
 
-init(autoreset=True)
 class SiteSearch:
-    def __init__(self, target_username, target_site):
+    def __init__(self, target_username, target_site, export_json=False):
         self.target = target_username
         self.target_site = target_site
+        self.export_json = export_json
+        self.results = []
 
+        # Result Directory Handling
+
+        self.save_dir = "results_search"    
+        self.PATH_FOR_RESULTS = f"{self.save_dir}/{self.target}_results.json"
+
+    # ----------- Loading Json Data -----------
     def load_data(self): 
         with open("sites.json", 'r') as f:
             self.loaded_data = json.load(f)
 
+    # ----------- PINGING SITES -----------
     def check_site(self, site_name, site_data):
-        url = site_data["url"]
-        final_url = url.replace("{}", self.target)
-        
+        url = site_data["url"] # Checking values in data file
+        final_url = url.replace("{}", self.target) # Changing {} to username
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        }
+
+        # ----------- MAIN PING LOGIC -----------
         try:
-            response = requests.get(final_url)
-            
-            # Check for success marker
+            response = requests.get(final_url, headers=headers, timeout=5)
             success_marker = site_data.get("success_text")
-            if success_marker:
-                if response.status_code == 200 and success_marker in response.text:
-                    print(f"{Fore.GREEN + '[+]'} Found {site_name}!!\n{final_url}")
-                else:
-                    print(f"{Fore.RED + '[-]'} Sorry, couldn't find anything in {site_name}")
-            
-            # Check for error marker
             error_marker = site_data.get("error_text")
-            if error_marker:
-                if response.status_code == 200 and error_marker not in response.text:
-                    print(f"{Fore.GREEN + '[+]'} Found {site_name}!!\n{final_url}")
-                else:
-                    print(f"{Fore.RED + '[-]'} Sorry, couldn't find anything in {site_name}")
-                    
+
+            # One Line Check
+            is_found = (success_marker in response.text) if success_marker else (error_marker not in response.text)
+
+            if response.status_code == 200 and is_found:
+                print(f"{Fore.GREEN}[+] Found {site_name}!!\n{final_url}")
+                self.results.append({
+                    "platform": site_name, 
+                    "url": final_url
+                })
+
+            else:
+                print(f"{Fore.RED}[-] Sorry, couldn't find anything in {site_name}")
+    
         except requests.exceptions.RequestException:
             print(Fore.RED + f"[!] Connection failed for {site_name}")
 
+    # ----------- Temporary Results -----------
+    def results_data(self):
+        if self.export_json: # Only touch the hard drive if the user opted in!
+            os.makedirs(self.save_dir, exist_ok=True) 
+            with open(self.PATH_FOR_RESULTS, 'w') as file:
+                json.dump(self.results, file, indent=4)
+            print(Fore.CYAN + f"[*] Results securely saved to {self.PATH_FOR_RESULTS}")
+        else:
+            print(Fore.CYAN + "[*] Scan complete. (Data not saved to disk)")
+
     def run_all(self):
-        self.load_data() # Load the JSON first
-        for site_name, site_data in self.loaded_data.items():
+        self.load_data()
+        for site_name, site_data in self.loaded_data.items():   
             
-            # The filter logic goes here!
+            # Filter For User_Input
             if self.target_site != "" and self.target_site.lower() != site_name.lower():
                 continue
 
-            # Tell the sniper to take the shot
+            # Chrcking sites
             self.check_site(site_name, site_data)
+        self.results_data()
